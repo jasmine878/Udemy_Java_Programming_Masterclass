60901abc99bede912e1a6c6636229d747bbc8d38
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-25 16:02:07 -0500

    Print my First Number

--- HelloChallenge/src/HelloChallenge.java
+++ HelloChallenge/src/HelloChallenge.java
@@ -1,5 +1,8 @@
 public class HelloChallenge {
     public static void main(String[] args) {
         System.out.println("Hello, Human!");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 }
