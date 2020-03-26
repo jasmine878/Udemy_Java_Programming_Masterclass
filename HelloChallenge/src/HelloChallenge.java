b14cfbc7e5636c82a417baf3620a750160690972
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-26 17:07:01 -0500

    Create and print the last variable

--- HelloChallenge/src/HelloChallenge.java
+++ HelloChallenge/src/HelloChallenge.java
@@ -7,8 +7,10 @@ public class HelloChallenge {
         int mySecondNumber = 12;
         int myThirdNumber = 6;
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
 
         System.out.println(myFirstNumber);
         System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
 }
