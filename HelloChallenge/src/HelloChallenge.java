d91b71ce81459755a5a534e4703d1e871c2cafba
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-26 16:57:21 -0500

    Create and print myTotal variable

--- HelloChallenge/src/HelloChallenge.java
+++ HelloChallenge/src/HelloChallenge.java
@@ -6,7 +6,9 @@ public class HelloChallenge {
         int myFirstNumber = 5;
         int mySecondNumber = 12;
         int myThirdNumber = 6;
-        
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
         System.out.println(myFirstNumber);
+        System.out.println(myTotal);
     }
 }
