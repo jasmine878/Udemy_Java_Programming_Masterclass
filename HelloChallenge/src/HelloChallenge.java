9f137d961a7219b447d736f775d36d88f71075f4
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-26 16:35:34 -0500

    Declare variables

--- HelloChallenge/src/HelloChallenge.java
+++ HelloChallenge/src/HelloChallenge.java
@@ -1,4 +1,7 @@
 public class HelloChallenge {
+    int mySecondNumber = 12;
+    int myThirdNumber = 6;
+    
     public static void main(String[] args) {
         System.out.println("Hello, Human!");
 
