ced4924610b883889907d4b4ff5955f0cca3c7e6
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-26 16:38:07 -0500

    Move variables inside the Main method

--- HelloChallenge/src/HelloChallenge.java
+++ HelloChallenge/src/HelloChallenge.java
@@ -1,11 +1,12 @@
 public class HelloChallenge {
-    int mySecondNumber = 12;
-    int myThirdNumber = 6;
-    
+
     public static void main(String[] args) {
         System.out.println("Hello, Human!");
 
         int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        
         System.out.println(myFirstNumber);
     }
 }
