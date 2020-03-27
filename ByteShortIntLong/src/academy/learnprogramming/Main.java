ead2823ab0da52d1c88c66d2288864d058cce87b
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-26 23:32:27 -0500

    Print min and max values of an Integer in Java

--- ByteShortIntLong/src/academy/learnprogramming/Main.java
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -3,6 +3,11 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int myValue = 10000;
+        int minIntValue = Integer.MIN_VALUE;
+        int maxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Minimum Int value is: " + minIntValue);
+        System.out.println("Maximum Int value is: " + maxIntValue);
     }
 }
