339ad733c1d1ca47456019994f5d66556bf36be7
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-28 13:10:11 -0500

    Convert a number from pounds to kilograms

---
+++ ByteShortIntLong/src/academy/learnprogramming/PoundsToKilograms.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class PoundsToKilograms {
+    public static void main(String[] args) {
+        int pounds = 200;
+        double kilograms = pounds * .45359237;
+
+        System.out.println("Converted kilograms = " + kilograms);
+    }
+}
