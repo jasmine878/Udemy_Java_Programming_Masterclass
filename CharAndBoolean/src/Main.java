f70a99535d74cd7c0d73b9d5028499dca98a9af1
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-28 15:32:55 -0500

    Print a char using its Unicode number

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    char letter = 'D';
+	    char unicode = '\u0044';
+
+        System.out.println(letter);
+        System.out.println(unicode);
+    }
+}
