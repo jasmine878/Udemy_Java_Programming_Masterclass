2207e925dd9503a0272b059c44a3aadf6bfcc630
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-28 11:06:04 -0500

    Print min and max values of a Foat in Java

---
+++ ByteShortIntLong/src/academy/learnprogramming/FloatAndDouble.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class FloatAndDouble {
+    public static void main(String[] args) {
+        float minFloat = Float.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+
+        System.out.println("Float minimum value = " + minFloat);
+        System.out.println("Float maximum value = " + maxFloat);
+
+
+    }
+}
