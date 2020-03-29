8b1b15ca15dbc118486ff115d359436e2cf58144
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-29 17:48:55 -0500

    Demonstrate a standard  if-then statement

--- Operators/src/Boolean.java
+++ Operators/src/Boolean.java
@@ -1,5 +1,9 @@
 public class Boolean {
     public static void main(String[] args) {
+        boolean isAlien = false;
 
+        if(isAlien == false) {
+            System.out.println("It is not an alien!");
+        }
     }
 }
