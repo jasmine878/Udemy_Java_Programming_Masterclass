2af07e415da910d26001d680283750ba8dbd8add
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-29 13:02:53 -0500

    Calculate an expression using the modulus operator

--- Operators/src/Main.java
+++ Operators/src/Main.java
@@ -17,5 +17,8 @@ public class Main {
 
         result = result / 5;
         System.out.println("20 / 5 = " + result);                            //expect 4
+
+        result = result % 3;
+        System.out.println("4 % 3 = " + result);                             //expect 1
     }
 }
