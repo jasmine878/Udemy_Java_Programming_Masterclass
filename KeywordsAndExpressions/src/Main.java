7b6433561c9d8be4f2ce6f419106cd0447ab2722
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-30 14:52:01 -0500

    Display an expression

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -3,5 +3,11 @@ public class Main {
         double kilometers = 100 * 1.609344;
 
         System.out.println("100 miles to kilometers is " + kilometers);
+
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
     }
 }
