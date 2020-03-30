3ad2cd62e6acbfac363885ee2146d77c1de736e5
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-30 14:58:06 -0500

    Given some code, Identify the expressions on each line

---
+++ KeywordsAndExpressions/src/ExpressionsChallenge.java
@@ -0,0 +1,10 @@
+public class ExpressionsChallenge {
+    //what are the expressions?
+
+    public static void main(String[] args) {
+        int score = 100;                                        //expression:  score = 100
+        if (score > 99) {                                       //expression:  score > 99
+            System.out.println("You got the high score!");      //expression:  "You got the high score!"
+        }
+    }
+}
