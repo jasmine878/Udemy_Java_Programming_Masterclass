82b60057bde8d204ea5f4b005cd6214a3dcdfa39
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-30 17:53:14 -0500

    Create a displayHighScorePosition class method

---
+++ Methods/src/MethodChallenge.java
@@ -0,0 +1,9 @@
+public class MethodChallenge {
+    public static void main(String[] args) {
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+        System.out.println(playerName + " managed to get into position " + playerPosition + " on the high score table");
+    }
+}
