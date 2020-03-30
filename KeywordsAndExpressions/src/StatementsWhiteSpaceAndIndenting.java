af94d18ef52dba138588e886c4a6eb03752a8fb6
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-30 15:05:06 -0500

    Create a few statements

---
+++ KeywordsAndExpressions/src/StatementsWhiteSpaceAndIndenting.java
@@ -0,0 +1,8 @@
+public class StatementsWhiteSpaceAndIndenting {
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+    }
+}
