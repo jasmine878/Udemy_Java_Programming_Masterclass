5c23edf772944ebca2bb82be225b2867f5226a6f
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-30 17:05:18 -0500

    Refactor the Method to return a value instead of printing to the screen

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -1,17 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        
-        calculateScore(true, 5_000, 5, 100);
 
-        calculateScore(true, 10_000, 8, 200);
+        int highScore = calculateScore(true, 5_000, 5, 100);
+
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(true, 10_000, 8, 200);
+
+        System.out.println("Your final score was " + highScore);
     }
 
-    public static void calculateScore( boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore( boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + levelCompleted * bonus;
 
-            System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
+
+        return -1;
     }
 }
