20070b598a98658f6c27f1567b341591e8bb179d
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-30 14:48:38 -0500

    Convert miles to kilometers

---
+++ KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double kilometers = 100 * 1.609344;
+
+        System.out.println("100 miles to kilometers is " + kilometers);
+    }
+}
