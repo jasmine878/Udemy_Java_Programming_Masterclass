acbd691b5a139ac7d21c608a5febbcde5de7ded1
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-30 18:08:44 -0500

    Create a calculateHighScorePosition class method

--- Methods/src/MethodChallenge.java
+++ Methods/src/MethodChallenge.java
@@ -6,4 +6,16 @@ public class MethodChallenge {
     public static void displayHighScorePosition(String playerName, int playerPosition) {
         System.out.println(playerName + " managed to get into position " + playerPosition + " on the high score table");
     }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore > 1000) {
+            return 1;
+        } else if (playerScore > 500) {
+            return 2;
+        } else if (playerScore > 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
 }
