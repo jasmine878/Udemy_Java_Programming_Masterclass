1c546e06f1ba594c6c87a99768956a77229b018f
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-30 15:30:59 -0500

    Create several variables

---
+++ KeywordsAndExpressions/src/IfKeywordAndCodeBlocks.java
@@ -0,0 +1,8 @@
+public class IfKeywordAndCodeBlocks {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 5_000;
+        int levelCompleted = 5;
+        int bonus = 100;
+    }
+}
