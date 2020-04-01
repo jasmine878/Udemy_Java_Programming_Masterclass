519c3b1f5981533bdebf4af69697b31f4254e423
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-31 22:40:50 -0500

    Correct conversion with division instead of multiplication

--- Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
+++ Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
@@ -10,6 +10,6 @@ public class SpeedConverter {
     public static long toMilesPerHour(double kilometersPerHour) {
         if (kilometersPerHour < 0) return -1;
 
-        return Math.round(kilometersPerHour * 1.609);
+        return Math.round(kilometersPerHour / 1.609);
     }
 }
