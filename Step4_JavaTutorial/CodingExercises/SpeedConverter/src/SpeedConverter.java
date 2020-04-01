867ddd7d0b48e5230f0345b3657127aa9e07b9aa
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-31 23:05:22 -0500

    Change mph variable type from double to long

--- Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
+++ Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
@@ -5,6 +5,9 @@ public class SpeedConverter {
         System.out.println(toMilesPerHour(-5.6));               //expect -1
         System.out.println(toMilesPerHour(25.42));              //expect 16
         System.out.println(toMilesPerHour(75.114));             //expect 47
+
+        printConversion(1.5);                     //expect 1.5 km/h = 1 mi/h
+
     }
 
     public static long toMilesPerHour(double kilometersPerHour) {
@@ -16,7 +19,7 @@ public class SpeedConverter {
     public static void printConversion(double kilometersPerHour) {
         if (kilometersPerHour < 0) System.out.println("Invalid Value");
         else {
-            double mph = toMilesPerHour(kilometersPerHour);
+            long mph = Math.round(kilometersPerHour / 1.609);
 
             System.out.println(kilometersPerHour + " km/h = " + mph + " mi/h");
         }
