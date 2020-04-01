3a5b75c7ef92b69b403f0633ff22fefb1b079648
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-01 08:24:30 -0500

    Print the remaining kiloBytes after the conversion to megaBytes

--- Step4_JavaTutorial/CodingExercises/MegaBytesConverter/src/MegaBytesConverter.java
+++ Step4_JavaTutorial/CodingExercises/MegaBytesConverter/src/MegaBytesConverter.java
@@ -1,17 +1,17 @@
 public class MegaBytesConverter {
     public static void main(String[] args) {
-        printMegaBytesAndKiloBytes(2500);                       //expect 2
-        printMegaBytesAndKiloBytes(-1024);                      //expect 0
-        printMegaBytesAndKiloBytes(5000);                       //expect 4
+        printMegaBytesAndKiloBytes(2500);                       //expect 2 and 452
+        printMegaBytesAndKiloBytes(-1024);                      //expect Invalid Value
+        printMegaBytesAndKiloBytes(5000);                       //expect 4 and 904
     }
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes) {
         if (kiloBytes < 0) System.out.println("Invalid Value");
         else {
-            long megaBytes = Math.round(Math.floor(kiloBytes / 1024));
+            int megaBytes = (int) Math.round(Math.floor(kiloBytes / 1024));
+            int remainingKiloBytes = kiloBytes % 1024;
 
-            System.out.println(megaBytes);
+            System.out.println(megaBytes + " and " + remainingKiloBytes);
         }
-
     }
 }
