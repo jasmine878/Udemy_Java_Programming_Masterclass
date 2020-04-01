05b4a04e80de7b951a5e9a97132c0b1b7b614b22
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-31 23:25:44 -0500

    Replace formula for mph with the toMilesPerHour method

--- Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
+++ Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
@@ -22,7 +22,7 @@ public class SpeedConverter {
     public static void printConversion(double kilometersPerHour) {
         if (kilometersPerHour < 0) System.out.println("Invalid Value");
         else {
-            long mph = Math.round(kilometersPerHour / 1.609);
+            long mph = toMilesPerHour((kilometersPerHour));
 
             System.out.println(kilometersPerHour + " km/h = " + mph + " mi/h");
         }
