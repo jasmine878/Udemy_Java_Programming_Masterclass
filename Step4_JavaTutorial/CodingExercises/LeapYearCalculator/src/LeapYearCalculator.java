5961918be98e4e837a4adf305c50a565fb3d38a0
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-01 10:11:37 -0500

    Move main method to a separate Class file

--- Step4_JavaTutorial/CodingExercises/LeapYearCalculator/src/LeapYearCalculator.java
+++ Step4_JavaTutorial/CodingExercises/LeapYearCalculator/src/LeapYearCalculator.java
@@ -1,20 +1,4 @@
 public class LeapYearCalculator {
-    public static void main(String[] args) {
-        System.out.println(isLeapYear(1600));                   //expect true
-        System.out.println(isLeapYear(2000));                   //expect true
-        System.out.println(isLeapYear(2400));                   //expect true
-
-        System.out.println(isLeapYear(1700));                   //expect false
-        System.out.println(isLeapYear(1800));                   //expect false
-        System.out.println(isLeapYear(1900));                   //expect false
-        System.out.println(isLeapYear(2100));                   //expect false
-        System.out.println(isLeapYear(2200));                   //expect false
-        System.out.println(isLeapYear(2300));                   //expect false
-        System.out.println(isLeapYear(2500));                   //expect false
-        System.out.println(isLeapYear(2600));                   //expect false
-        System.out.println(isLeapYear(-1600));                  //expect false
-        System.out.println(isLeapYear(2017));                   //expect false
-    }
 
      public static boolean isLeapYear(int year) {
         if (year < 1 || year > 9999) return false;
---
+++ Step4_JavaTutorial/CodingExercises/LeapYearCalculator/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LeapYearCalculator.isLeapYear(1600));                   //expect true
+        System.out.println(LeapYearCalculator.isLeapYear(2000));                   //expect true
+        System.out.println(LeapYearCalculator.isLeapYear(2400));                   //expect true
+
+        System.out.println(LeapYearCalculator.isLeapYear(1700));                   //expect false
+        System.out.println(LeapYearCalculator.isLeapYear(1800));                   //expect false
+        System.out.println(LeapYearCalculator.isLeapYear(1900));                   //expect false
+        System.out.println(LeapYearCalculator.isLeapYear(2100));                   //expect false
+        System.out.println(LeapYearCalculator.isLeapYear(2200));                   //expect false
+        System.out.println(LeapYearCalculator.isLeapYear(2300));                   //expect false
+        System.out.println(LeapYearCalculator.isLeapYear(2500));                   //expect false
+        System.out.println(LeapYearCalculator.isLeapYear(2600));                   //expect false
+        System.out.println(LeapYearCalculator.isLeapYear(-1600));                  //expect false
+        System.out.println(LeapYearCalculator.isLeapYear(2017));                   //expect false
+    }
+}
