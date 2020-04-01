e66fa39694bfec5ab430f9ddb084ff7a595059d1
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-31 21:19:27 -0500

    convert several values using the toMilesPerHour method

--- Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
+++ Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
@@ -1,6 +1,10 @@
 public class SpeedConverter {
     public static void main(String[] args) {
-
+        System.out.println(toMilesPerHour(1.5));                //expect 1
+        System.out.println(toMilesPerHour(10.25));              //expect 6
+        System.out.println(toMilesPerHour(-5.6));               //expect -1
+        System.out.println(toMilesPerHour(25.42));              //expect 16
+        System.out.println(toMilesPerHour(75.114));             //expect 47
     }
 
     public static long toMilesPerHour(double kilometersPerHour) {
