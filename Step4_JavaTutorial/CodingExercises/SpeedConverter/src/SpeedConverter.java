40534b177a8d757e61d8d3aa2b951fd6e9d3d86e
Tae Hui Chang <jasmine878@yahoo.com>
2020-03-31 21:13:13 -0500

    Write a formula to convert Kilometers to Miles per Hour

--- Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
+++ Step4_JavaTutorial/CodingExercises/SpeedConverter/src/SpeedConverter.java
@@ -3,7 +3,9 @@ public class SpeedConverter {
 
     }
 
-    public static void toMilesPerHour(double kilometersPerHour) {
-        
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) return -1;
+
+        return Math.round(kilometersPerHour * 1.609);
     }
 }
