fe2f71cd76f790caa3df81e7c106618353f86140
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-01 14:46:20 -0500

    Modify difference operand

--- Step4_JavaTutorial/CodingExercises/DecimalComparator/src/DecimalComparator.java
+++ Step4_JavaTutorial/CodingExercises/DecimalComparator/src/DecimalComparator.java
@@ -2,15 +2,16 @@ public class DecimalComparator {
     public static void main(String[] args) {
         System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));              //expect true
         System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));                 //expect false
+        System.out.println(areEqualByThreeDecimalPlaces(3.176, 3.175));                 //expect false
         System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));                     //expect true
         System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));                //expect false
     }
 
     public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
         double difference = Math.abs(num1 - num2);
-        System.out.println("difference = " + difference);
+//        System.out.println("difference = " + difference);
 
-        if (difference < .009) return true;
+        if (difference < .0009) return true;
         return false;
     }
 }
