75010425107ea28f47ecf733f5cf95e458c973ff
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-02 20:47:06 -0500

    Create a getDaysInMonth method demonstrating a Switch statement

--- Step5_ControlFlowStatements/CodingExercises/NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
+++ Step5_ControlFlowStatements/CodingExercises/NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
@@ -4,6 +4,12 @@ public class NumberOfDaysInMonth {
         System.out.println(isLeapYear(1600));           //expect true
         System.out.println(isLeapYear(2017));           //expect false
         System.out.println(isLeapYear(2000));           //expect true
+
+        System.out.println(getDaysInMonth(1, 2020));        //expect 31
+        System.out.println(getDaysInMonth(2, 2020));        //expect 29
+        System.out.println(getDaysInMonth(2, 2018));        //expect 28
+        System.out.println(getDaysInMonth(-1, 2020));       //expect -1
+        System.out.println(getDaysInMonth(1, -2020));       //expect -1
     }
     public static boolean isLeapYear(int year) {
         if (year < 1 || year > 9999) return false;
@@ -12,5 +18,40 @@ public class NumberOfDaysInMonth {
         if (year % 400 == 0) return true;
 
         return false;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12) return -1;
+        if (year < 1 || year > 9999) return -1;
+
+        switch (month) {
+            case 1:
+                return 31;
+            case 2:
+                if (isLeapYear(year)) return 29;
+                else return 28;
+            case 3:
+                return 31;
+            case 4:
+                return 30;
+            case 5:
+                return 31;
+            case 6:
+                return 30;
+            case 7:
+                return 31;
+            case 8:
+                return 31;
+            case 9:
+                return 30;
+            case 10:
+                return 31;
+            case 11:
+                return 30;
+            case 12:
+                return 31;
+            default:
+                return -1;
+        }
     }
 }
