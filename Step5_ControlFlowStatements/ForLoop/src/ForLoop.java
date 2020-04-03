a5dabc00ee2523bd33fe0f98a8df4fe515a1d11c
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-02 22:59:02 -0500

    Print Interest using a For Loop using a decreasing variable

--- Step5_ControlFlowStatements/ForLoop/src/ForLoop.java
+++ Step5_ControlFlowStatements/ForLoop/src/ForLoop.java
@@ -10,7 +10,13 @@ public class ForLoop {
             System.out.println("Loop " + i + " Hello!");
         }
 
-        for (int i = 2; i <= 5; i++) {
+        for (int i = 2; i <= 7; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+        }
+
+        System.out.println('\n');
+        
+        for (int i = 8; i >= 2; i--) {
             System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
         }
     }
