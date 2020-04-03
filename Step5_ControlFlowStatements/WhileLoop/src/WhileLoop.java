5abf67bd241dbe968f90d985565383f244e03192
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-03 11:33:16 -0500

    Demonstrate a While Loop

---
+++ Step5_ControlFlowStatements/WhileLoop/src/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+    }
+
+}
