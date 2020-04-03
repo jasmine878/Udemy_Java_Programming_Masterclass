87c0cae4c8772ab38f5cb9ffc1ecfb1a486a2a4a
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-02 22:47:20 -0500

    Change variable tag in For Loop to an int variable

--- Step5_ControlFlowStatements/ForLoop/src/ForLoop.java
+++ Step5_ControlFlowStatements/ForLoop/src/ForLoop.java
@@ -10,7 +10,7 @@ public class ForLoop {
             System.out.println("Loop " + i + " Hello!");
         }
 
-        for (double i = 2; i <= 5; i++) {
+        for (int i = 2; i <= 5; i++) {
             System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
         }
     }
