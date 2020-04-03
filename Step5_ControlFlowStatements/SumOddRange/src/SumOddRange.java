c22a9dbddaff0247f965a2dbd3f4770ef53e4b5e
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-03 11:13:03 -0500

    Create a sumOdd method

--- Step5_ControlFlowStatements/SumOddRange/src/SumOddRange.java
+++ Step5_ControlFlowStatements/SumOddRange/src/SumOddRange.java
@@ -1,6 +1,11 @@
 public class SumOddRange {
     public static void main(String[] args) {
-        
+        System.out.println(sumOdd(1, 100));                 //expect 2500
+        System.out.println(sumOdd(-1, 100));                //expect -1
+        System.out.println(sumOdd(100, 100));               //expect 0
+        System.out.println(sumOdd(13, 13));                 //expect 13
+        System.out.println(sumOdd(100, -100));              //expect -1
+        System.out.println(sumOdd(100, 1000));              //expect 247500
     }
 
     public static boolean isOdd(int number) {
@@ -8,5 +13,17 @@ public class SumOddRange {
         if (number % 2 == 1) return true;
 
         return false;
+    }
+
+    public static int sumOdd(int start, int end) {
+        if (start < 0 || end < 0) return -1;
+
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) sum += i;
+        }
+
+        return sum;
     }
 }
