17d3f0e0e868a418fc4a3596042d59f0e704b65e
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-03 11:07:05 -0500

    Create an isOdd method

---
+++ Step5_ControlFlowStatements/SumOddRange/src/SumOddRange.java
@@ -0,0 +1,12 @@
+public class SumOddRange {
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean isOdd(int number) {
+        if (number <= 0) return false;
+        if (number % 2 == 1) return true;
+
+        return false;
+    }
+}
