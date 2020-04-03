129c4f82eb674c3dc009a50f244418454d290fd5
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-03 12:26:50 -0500

    Utilize isEvenNumber method inside the While Loop

--- Step5_ControlFlowStatements/WhileLoop/src/WhileLoop.java
+++ Step5_ControlFlowStatements/WhileLoop/src/WhileLoop.java
@@ -1,18 +1,36 @@
 public class WhileLoop {
     public static void main(String[] args) {
-        int count = 1;
+//        int count = 1;
+//
+//        while (count != 6) {
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
+//        System.out.println("\n");
+//        count = 1;
+//
+//        do {
+//            System.out.println("Count value is " + count);
+//            count++;
+//        } while(count != 6);
+//
+        int start = 4;
+        int finishNumber = 20;
 
-        while (count != 6) {
-            System.out.println("Count value is " + count);
-            count++;
+        while (start <= finishNumber) {
+
+            if (isEvenNumber(start)) {
+                System.out.println(start + " is an even number");
+            }
+
+            start++;
         }
-        System.out.println("\n");
-        count = 1;
+    }
 
-        do {
-            System.out.println("Count value is " + count);
-            count++;
-        } while(count != 6);
+    public static boolean isEvenNumber(int number) {
+        if (number % 2 == 0) return true;
+
+        return false;
     }
 
 }
