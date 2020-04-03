f65c06acad9adc352c9f6b86c5d776cf0cc4619d
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-03 15:00:53 -0500

    Create a method called sumDigit

---
+++ Step5_ControlFlowStatements/DigitSum/src/DigitSum.java
@@ -0,0 +1,22 @@
+public class DigitSum {
+    public static void main(String[] args) {
+        System.out.println(sumDigit(125));              //expect 8
+        System.out.println(sumDigit(1));                //expect -1
+    }
+
+    public static int sumDigit(int number) {
+        if (number < 10) return -1;
+
+        int remainingDigits = number;
+        int sum = 0;
+        int lastDigit = 0;
+
+        while (remainingDigits > 0) {
+            lastDigit = remainingDigits % 10;
+            sum += lastDigit;
+            remainingDigits /= 10;
+        }
+
+        return sum;
+    }
+}
