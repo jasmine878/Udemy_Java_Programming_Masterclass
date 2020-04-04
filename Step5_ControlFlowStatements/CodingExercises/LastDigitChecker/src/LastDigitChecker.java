8d91fe11924f77547424ca92e28e5e732f9267a6
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-04 14:13:19 -0500

    Create an isValid helper method

---
+++ Step5_ControlFlowStatements/CodingExercises/LastDigitChecker/src/LastDigitChecker.java
@@ -0,0 +1,15 @@
+public class LastDigitChecker {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasSameLastDigit(int num1, int num2, int num3) {
+        return false;
+    }
+
+    public static boolean isValid(int num) {
+        if (num >= 10 && num <= 1000) return true;
+        
+        return false;
+    }
+}
