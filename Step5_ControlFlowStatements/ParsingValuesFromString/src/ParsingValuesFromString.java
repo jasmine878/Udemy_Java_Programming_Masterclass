46a57d45d246436e4dcc94c1fc038ada1ef5e746
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-05 15:31:04 -0500

    Practice using the parseInt method

---
+++ Step5_ControlFlowStatements/ParsingValuesFromString/src/ParsingValuesFromString.java
@@ -0,0 +1,18 @@
+public class ParsingValuesFromString {
+    public static void main(String[] args) {
+        String numberAsString = "2018";
+        System.out.println("number as a string: " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+        System.out.println("\n");
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString " + numberAsString);
+        System.out.println("number " + number);
+    }
+
+
+}
