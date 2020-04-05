08e41df602a9ef3fdfcc3f16aa2e69b358c718bf
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-05 15:47:52 -0500

    Practice using the parseDouble method

--- Step5_ControlFlowStatements/ParsingValuesFromString/src/ParsingValuesFromString.java
+++ Step5_ControlFlowStatements/ParsingValuesFromString/src/ParsingValuesFromString.java
@@ -4,7 +4,9 @@ public class ParsingValuesFromString {
         System.out.println("number as a string: " + numberAsString);
 
         int number = Integer.parseInt(numberAsString);
+        double number2 = Double.parseDouble(numberAsString);
         System.out.println("number = " + number);
+        System.out.println("number2 = " + number2);
         System.out.println("\n");
 
         numberAsString += 1;
