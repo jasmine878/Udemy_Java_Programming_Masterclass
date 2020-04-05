31561b64c8359a1404896990cb12f12cb39b55b7
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-05 17:43:16 -0500

    Create a get LargestPrime method

--- Step5_ControlFlowStatements/CodingExercises/LargestPrime/src/LargestPrime.java
+++ Step5_ControlFlowStatements/CodingExercises/LargestPrime/src/LargestPrime.java
@@ -1,14 +1,31 @@
 public class LargestPrime {
     public static void main(String[] args) {
-        System.out.println(isPrime(14));            //expect false;
-        System.out.println(isPrime(11));             //expect true;
+//        System.out.println(isPrime(14));                //expect false;
+//        System.out.println(isPrime(11));                //expect true;
+//        System.out.println("\n");
+
+        System.out.println(getLargestPrime(21));        //expect 7
+        System.out.println(getLargestPrime(217));       //expect 31
+        System.out.println(getLargestPrime(0));         //expect -1
+        System.out.println(getLargestPrime(45));        //expect 5
+        System.out.println(getLargestPrime(-1));        //expect -1
     }
 
-//    public static int getLargestPrime(int number) {
-//        if (number < 0) return -1;
-//
-//        int currentNumber = number / 2;
-//    }
+    public static int getLargestPrime(int number) {
+        if (number < 0) return -1;
+
+        int currentNumber = number / 2;
+
+        while (currentNumber > 1) {
+            if (isPrime(currentNumber) && number % currentNumber == 0) {
+//                System.out.println("Prime number is: " + currentNumber);
+                return currentNumber;
+            }
+            currentNumber--;
+        }
+
+        return -1;
+    }
 
     public static boolean isPrime(int number) {
         if (number <= 1) return false;
