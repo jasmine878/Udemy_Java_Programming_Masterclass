1a7c5ec60a14a406f88e2c87deedfd9849165389
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-04 20:16:16 -0500

    Add more test cases to the printFactors method

--- Step5_ControlFlowStatements/CodingExercises/AllFactors/src/AllFactors.java
+++ Step5_ControlFlowStatements/CodingExercises/AllFactors/src/AllFactors.java
@@ -1,9 +1,13 @@
 public class AllFactors {
     public static void main(String[] args) {
         printFactors(6);                    //expect 1, 2, 3, 6
+        printFactors(32);                   //expect 1, 2, 4, 8, 16, 32
+        printFactors(10);                   //expect 1, 2, 5, 10
+        printFactors(-1);                   //expect -1
     }
 
     public static void printFactors(int number) {
+        System.out.println("The Factors of " + number + " are: ");
         if (number < 1) {
             System.out.println("Invalid Value");
         } else {
@@ -14,6 +18,6 @@ public class AllFactors {
                 currentNum ++;
             }
         }
-
+        System.out.println("\n");
     }
 }
