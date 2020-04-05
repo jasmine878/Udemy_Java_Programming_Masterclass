3c2b9f116c07358fd27c7541f3f31e93caef5bf4
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-05 17:15:08 -0500

    Solve can Pack method using a nested for loop

--- Step5_ControlFlowStatements/CodingExercises/FlourPacker/src/FlourPacker.java
+++ Step5_ControlFlowStatements/CodingExercises/FlourPacker/src/FlourPacker.java
@@ -5,13 +5,17 @@ public class FlourPacker {
         System.out.println(canPack(0, 5, 4));               //expect true;
         System.out.println(canPack(2, 2, 11));              //expect true
         System.out.println(canPack(-3, 2, 12));             //expect false;
+        System.out.println(canPack(2, 1, 5));               //expect true;
     }
 
     public static boolean canPack(int bigCount, int smallCount, int goal) {
         if (bigCount < 0 || smallCount < 0) return false;
 
-        if (bigCount * 5 > goal) return false;
-        if (bigCount * 5 + smallCount >= goal) return true;
+       for (int i = 0; i <= bigCount; i++) {
+           for (int j = 0; j <= smallCount; j++) {
+               if (i * 5 + j == goal) return true;
+           }
+       }
 
         return false;
     }
