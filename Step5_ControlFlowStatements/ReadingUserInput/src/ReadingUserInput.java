b21a35b3a36d697d4f30cbf1790fa05081ec1ebf
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-05 23:31:10 -0500

    Read user input birth year from the console

--- Step5_ControlFlowStatements/ReadingUserInput/src/ReadingUserInput.java
+++ Step5_ControlFlowStatements/ReadingUserInput/src/ReadingUserInput.java
@@ -4,12 +4,16 @@ public class ReadingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine();
+
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
 
-        System.out.println("Your name is " + name);
+        int age = 2020 - yearOfBirth;
+
+        System.out.println("Your name is " + name + ", and you are " + age + " years old");
         scanner.close();
     }
-
-
 }
