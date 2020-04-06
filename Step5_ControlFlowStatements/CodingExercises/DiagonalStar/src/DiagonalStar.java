b930504b2a6618ec53bff169c0663d5ae9f05e79
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-05 22:18:14 -0500

    Create a printSquareStar method

---
+++ Step5_ControlFlowStatements/CodingExercises/DiagonalStar/src/DiagonalStar.java
@@ -0,0 +1,44 @@
+public class DiagonalStar {
+    public static void main(String[] args) {
+//        printSquareStar(5);
+        //expect
+        //          *****
+        //          ** **
+        //          * * *
+        //          ** **
+        //          *****
+
+        printSquareStar(8);
+        //expect
+        //          ********
+        //          **    **
+        //          * *  * *
+        //          *  **  *
+        //          *  **  *
+        //          * *  * *
+        //          **    **
+        //          ********
+    }
+
+    public static void printSquareStar(int number) {
+        if (number < 5) System.out.println("Invalid Value");
+        else {
+            boolean printStar = false;
+
+            for (int row = 1; row <= number; row++) {
+                for (int col = 1; col <= number; col++) {
+                    if (row == 1 || row == number) printStar = true;
+                    if (col == 1 || col == number) printStar = true;
+                    if (row == col) printStar = true;
+                    if (col == number - row + 1) printStar = true;
+
+                    if (printStar == true) {
+                        System.out.print("*");
+                        printStar = false;
+                    } else System.out.print(" ");
+                }
+                if (row < number) System.out.println();
+            }
+        }
+    }
+}
