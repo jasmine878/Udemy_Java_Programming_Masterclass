846dc7117e00332ac2343934a5cba9fcf59a4ea4
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-07 10:24:49 -0500

    Create a class method to set the class variable

--- Step6_OOP_Part1/Classes/src/Main.java
+++ Step6_OOP_Part1/Classes/src/Main.java
@@ -3,5 +3,11 @@ public class Main {
     public static void main(String[] args) {
 	    Car porsche = new Car();            //we created an object porsche based on template Car
         Car holden = new Car();             //we created another object
+
+        System.out.println("Model is " + porsche.getModel());
+
+        porsche.setModel("Carrera");
+
+        System.out.println("Model is " + porsche.getModel());
     }
 }
