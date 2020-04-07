39d5447cbca3fdf6a13ff93ce90ef5fe4b4026b9
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-06 22:08:31 -0500

    Move main method to a separate Class file

--- Step5_ControlFlowStatements/CodingExercises/InputCalculator/src/InputCalculator.java
+++ Step5_ControlFlowStatements/CodingExercises/InputCalculator/src/InputCalculator.java
@@ -1,13 +1,6 @@
 import java.util.Scanner;
 
 public class InputCalculator {
-    public static void main(String[] args) {
-        inputThenPrintSumAndAverage();
-
-        //1, 2, 3, 4, 5                     //expect SUM = 15 AVG = 3
-        //hello                             //expect SUM = 0 AVG = 0
-        //-1, 5, 7, bg 9                    //expect SUM = 11 AVG = 4
-    }
 
     public static void inputThenPrintSumAndAverage() {
         int sum = 0;
---
+++ Step5_ControlFlowStatements/CodingExercises/InputCalculator/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        InputCalculator.inputThenPrintSumAndAverage();
+
+        //1, 2, 3, 4, 5                     //expect SUM = 15 AVG = 3
+        //hello                             //expect SUM = 0 AVG = 0
+        //-1, 5, 7, bg 9                    //expect SUM = 11 AVG = 4
+    }
+}
