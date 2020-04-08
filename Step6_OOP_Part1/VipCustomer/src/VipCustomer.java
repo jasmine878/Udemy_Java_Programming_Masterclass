be53be22b4d73643f1e68ec294368ef1663aa177
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-08 17:26:22 -0500

    Create a VipCustomer class with multiple Constructors

---
+++ Step6_OOP_Part1/VipCustomer/src/VipCustomer.java
@@ -0,0 +1,37 @@
+public class VipCustomer {
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public VipCustomer() {
+        this("default Name", 0, "default email");
+    }
+
+    public VipCustomer(String name, double creditLimit) {
+        this(name, creditLimit, "default email");
+    }
+
+    public VipCustomer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public static void main(String[] args) {
+        VipCustomer Stan = new VipCustomer();
+
+        System.out.println(Stan.name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
