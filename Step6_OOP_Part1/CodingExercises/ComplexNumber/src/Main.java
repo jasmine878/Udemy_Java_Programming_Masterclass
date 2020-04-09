85e96bf4b36d31975b4cf0a74754b9e3899ac863
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-08 23:04:23 -0500

    Perform addition and subtraction with Complex numbers

--- Step6_OOP_Part1/CodingExercises/ComplexNumber/src/Main.java
+++ Step6_OOP_Part1/CodingExercises/ComplexNumber/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
 
+        one.add(1, 1);
+
+        System.out.println("one.real= " + one.getReal());                       //expect 2.0
+        System.out.println("one.imaginary= " + one.getImaginary());             //expect 2.0
+
+        one.subtract(number);
+
+        System.out.println("one.real= " + one.getImaginary());                  //expect -.5
+        System.out.println("one.imaginary= " + one.getImaginary());             //expect 3.5
+
+        number.subtract(one);
+
+        System.out.println("number.real= " + number.getReal());                 //expect 3.0
+        System.out.println("number.imaginary= " + number.getImaginary());       //expect -5.0
     }
 }
