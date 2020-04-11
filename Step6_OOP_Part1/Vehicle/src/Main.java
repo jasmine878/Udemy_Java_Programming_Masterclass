11ad865dea68785136d21d3670f1358b19e977f8
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-10 20:44:57 -0500

    Create an instance of Outlander Class

--- Step6_OOP_Part1/Vehicle/src/Main.java
+++ Step6_OOP_Part1/Vehicle/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Outlander outlander = new Outlander(36);
 
+        outlander.steer(45);
+        outlander.accelerate(30);
+        System.out.println();
+
+        outlander.accelerate(20);
     }
 }
