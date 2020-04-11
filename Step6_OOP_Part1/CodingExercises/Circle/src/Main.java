7f1ddbad42c0fb93df075cfa9f3c3476c9867489
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-10 21:59:46 -0500

    Create a circle and a cylinder instance

--- Step6_OOP_Part1/CodingExercises/Circle/src/Main.java
+++ Step6_OOP_Part1/CodingExercises/Circle/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        Circle circle = new Circle(3.75);
 
+        System.out.println("circle.radius= " + circle.getRadius());         //expect 3.75
+        System.out.println("circle.area= " + circle.getArea());             //expect 44.178
+        System.out.println();
+
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+
+        System.out.println("cylinder.radius= " + cylinder.getRadius());     //expect 5.55
+        System.out.println("cylinder.height= " + cylinder.getHeight());     //expect 7.25
+        System.out.println("cylinder.area= " + cylinder.getArea());         //expect 96.7689
+        System.out.println("cylinder.volume= " + cylinder.getVolume());     //expect 701.5745
     }
 }
