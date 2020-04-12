ac4b80fab455cbb5d99b75d98a1170ae82672970
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-12 18:34:44 -0500

    Create various printer instances and invoke its methods

--- Step6_OOP_Part1/EncapsulationChallenge/src/Printer.java
+++ Step6_OOP_Part1/EncapsulationChallenge/src/Printer.java
@@ -14,7 +14,24 @@ public class Printer {
     }
 
     public static void main(String[] args) {
+        Printer printer = new Printer(50, true);
+        System.out.println("initial page count = " + printer.getNumberPagesPrinted());
 
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getNumberPagesPrinted());
+
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getNumberPagesPrinted());
+        System.out.println();
+
+        Printer printer2 = new Printer(50, false);
+        System.out.println("initial page count = " + printer2.getNumberPagesPrinted());
+
+        pagesPrinted = printer2.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer2.getNumberPagesPrinted());
+
+        pagesPrinted = printer2.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer2.getNumberPagesPrinted());
     }
 
     public double getTonerLevel() {
