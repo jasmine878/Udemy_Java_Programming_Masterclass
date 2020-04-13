282e199c46edefa0795104c0baec2767d42f879d
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-12 23:25:20 -0500

    Create a Car class

---
+++ Step6_OOP_Part1/PolymorphismChallenge/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Car {
+    private String name;
+    private boolean engine;
+    private int cylinders;
+    private int wheels;
+
+    public Car(String name, int cylinders) {
+        this.name = name;
+        this.cylinders = cylinders;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate";
+    }
+
+    public String brake() {
+        return "Car -> brake";
+    }
+}
