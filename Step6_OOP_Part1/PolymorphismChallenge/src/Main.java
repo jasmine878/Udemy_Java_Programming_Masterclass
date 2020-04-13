2ee3d9a18498e4884ccac8fad985dd553a328109
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-12 23:50:10 -0500

    Create a Ford child class

--- Step6_OOP_Part1/PolymorphismChallenge/src/Main.java
+++ Step6_OOP_Part1/PolymorphismChallenge/src/Main.java
@@ -67,3 +67,25 @@ class Mitsubishi extends Car {
         return "Mitsubishi -> brake()";
     }
 }
+
+class Ford extends Car {
+    public Ford(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford -> brake()";
+    }
+}
+
