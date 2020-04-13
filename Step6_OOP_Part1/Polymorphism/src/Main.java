43c8bf0ab67762a40bae0ff9f230d2de3abe67e1
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-12 22:54:37 -0500

    Create a randomMovie method in the Main class to generate a random movie

--- Step6_OOP_Part1/Polymorphism/src/Main.java
+++ Step6_OOP_Part1/Polymorphism/src/Main.java
@@ -1,60 +1,93 @@
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to takeover planet earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try to escape a maze";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial Forces try to take over the Galaxy";
+    }
+}
+
+class ForgetableMovieType extends Movie {
+    public ForgetableMovieType() {
+        super("Forgetable");
+    }
+
+    //no plot method
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        
     }
 
-    public class Movie {
-        private String name;
+    //purpose of this method is to return a random Movie
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5 + 1);
+        System.out.println("Random number generated is: " + randomNumber);
 
-        public Movie(String name) {
-            this.name = name;
-        }
-
-        public String plot() {
-            return "No plot here";
-        }
-    }
-
-    public class Jaws extends Movie {
-        public Jaws() {
-            super("Jaws");
-        }
-
-        public String plot() {
-            return "A shark eats lots of people";
-        }
-    }
-
-    public class IndependenceDay extends Movie {
-        public IndependenceDay() {
-            super("Independence Day");
-        }
-
-        @Override
-        public String plot() {
-            return "Aliens attempt to takeover planet earth";
-        }
-    }
-
-    public class MazeRunner extends Movie {
-        public MazeRunner() {
-            super("Maze Runner");
-        }
-
-        @Override
-        public String plot() {
-            return "Kids try to escape a maze";
-        }
-    }
-
-    public class StarWars extends Movie {
-        public StarWars() {
-            super("Star Wars");
-        }
-
-        @Override
-        public String plot() {
-            return "Imperial Forces try to take over the Galaxy";
+        switch(randomNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new ForgetableMovieType();
+            default:
+                return null;
         }
     }
 }
