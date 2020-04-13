924a950c11c5ab5e93f670f88c4bcc8e89238b81
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-12 21:51:15 -0500

    Create a Movie class

---
+++ Step6_OOP_Part1/Polymorphism/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public class Movie {
+        private String name;
+
+        public Movie(String name) {
+            this.name = name;
+        }
+
+        public String plot() {
+            return "No plot here";
+        }
+    }
+}
