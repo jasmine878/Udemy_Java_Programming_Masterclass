03855e6607becf41e81796c4a57ea6cc2165c8de
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-13 23:09:47 -0500

    Create a method that calculates the average value of a user inputs using an array

--- Step8_Arrays/Arrays/src/Arrays.java
+++ Step8_Arrays/Arrays/src/Arrays.java
@@ -10,7 +10,17 @@ public class Arrays {
         for (int i = 0; i < number; i++) {
             values[i] = scanner.nextInt();
         }
-
+        System.out.println("The average value of inputs is " + getAverage(values));
         return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        return (double) sum / (double) array.length;
     }
 }
