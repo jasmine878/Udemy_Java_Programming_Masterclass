262c6b58d17acd90e3894f3c328dce4ee6ead528
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-13 22:14:39 -0500

    Create an array using a for loop to assign values

--- Step8_Arrays/Arrays/src/Main.java
+++ Step8_Arrays/Arrays/src/Main.java
@@ -8,5 +8,13 @@ public class Main {
 
         int[] myIntArray2 = {1, 2, 3, 4, 5, 6};
         System.out.println(myIntArray2[5]);             //expect 6
+        System.out.println();
+
+        int[] myIntArray3 = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            myIntArray3[i] = i * 10;
+        }
+        System.out.println(myIntArray3[5]);             //expect 50
     }
 }
