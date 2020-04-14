9f91e565428848788ff6fab0f20dcde4d2144cb0
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-13 22:06:16 -0500

    Initialize an array using the new keyword

---
+++ Step8_Arrays/Arrays/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] myIntArray1 = new int[10];
+
+        myIntArray1[5] = 50;
+        System.out.println(myIntArray1[5]);             //expect 50
+        System.out.println();
+        
+    }
+}
