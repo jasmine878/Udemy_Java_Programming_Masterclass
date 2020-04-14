9e5a5f7c407dfee5ebd4681977fa7660adf9fd7d
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-14 17:07:58 -0500

    Use the Arrays.copyOf method to copy an array

--- Step8_Arrays/SortedArrayChallenge/src/Main.java
+++ Step8_Arrays/SortedArrayChallenge/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -30,11 +31,13 @@ public class Main {
     }
 
     public static int[] sortIntegers(int[] array) {
-        int[] sortedArray = new int[array.length];
+//        int[] sortedArray = new int[array.length];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            sortedArray[i] = array[i];
+//        }
 
-        for (int i = 0; i < array.length; i++) {
-            sortedArray[i] = array[i];
-        }
+        int[] sortedArray = Arrays.copyOf(array, array.length);
 
         boolean notSorted = true;
 
