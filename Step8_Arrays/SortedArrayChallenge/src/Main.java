7b775ab22981f829c35cbe0cb2845c27b5f28516
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-14 16:41:41 -0500

    Create a sortIntegers method to return a sorted array

--- Step8_Arrays/SortedArrayChallenge/src/Main.java
+++ Step8_Arrays/SortedArrayChallenge/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         int[] myIntegers = getIntegers(5);
         printArray(myIntegers);
+        printArray(sortIntegers(myIntegers));
     }
 
     public static int[] getIntegers(int arrayCapacity) {
@@ -25,10 +26,33 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i] + " is at index " + i);
         }
+        System.out.println();
     }
 
     public static int[] sortIntegers(int[] array) {
-        return array;
+        int[] sortedArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+
+        boolean notSorted = true;
+
+        while(notSorted) {
+            notSorted = false;
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                int num1 = sortedArray[i];
+                int num2 = sortedArray[i + 1];
+
+                if (num1 < num2) {
+                    sortedArray[i] = num2;
+                    sortedArray[i + 1] = num1;
+                    notSorted = true;
+                }
+            }
+        }
+
+        return sortedArray;
     }
 
 
