57ca63b397313327d597ed062c74db3ea2cba295
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-15 00:26:10 -0500

    Create a reverse array method

---
+++ Step8_Arrays/ReverseArrayChallenge/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5};
+        reverse(array1);
+        System.out.println();
+        
+        int[] array2 = {55, 20, 1, 33, 17};
+        reverse(array2);
+    }
+
+    private static void reverse(int[] array) {
+        int[] reversedArr = new int[array.length];
+        int swappedIndex = array.length - 1;
+
+        for (int i = 0; i < array.length; i++) {
+            reversedArr[i] = array[swappedIndex];
+            swappedIndex--;
+        }
+        System.out.println("Original array is: " + Arrays.toString(array));
+        System.out.println("Reversed array is: " + Arrays.toString(reversedArr));
+    }
+}
