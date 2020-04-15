9d0f1b02d21551467906ada7e87acd6755b1671a
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-15 00:41:25 -0500

    Create another reverse method that loops the array and swaps halfway

--- Step8_Arrays/ReverseArrayChallenge/src/Main.java
+++ Step8_Arrays/ReverseArrayChallenge/src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         reverse(array1);
         System.out.println();
 
-//        int[] array2 = {55, 20, 1, 33, 17};
-//        reverse(array2);
+        int[] array2 = {55, 20, 1, 33, 17};
+        reverseHalf(array2);
     }
 
     private static void reverse(int[] array) {
@@ -21,5 +21,24 @@ public class Main {
         }
         System.out.println("Original array is: " + Arrays.toString(array));
         System.out.println("Reversed array is: " + Arrays.toString(reversedArr));
+    }
+
+    private static void reverseHalf(int[] array) {
+        int[] arrayCopy = Arrays.copyOf(array, array.length);
+
+        int swappedIndex = array.length - 1;
+        int midIdx = array.length / 2;
+
+        for (int i = 0; i < midIdx; i++) {
+            int currentElement = array[i];
+
+            array[i] = array[swappedIndex];
+            array[swappedIndex] = currentElement;
+            swappedIndex--;
+            System.out.println(Arrays.toString(array));
+        }
+
+        System.out.println("Original array is: " + Arrays.toString(arrayCopy));
+        System.out.println("Reversed array is: " + Arrays.toString(array));
     }
 }
