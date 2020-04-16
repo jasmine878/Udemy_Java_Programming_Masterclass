f4d86071218de41810bafc26ddef7920b8c604bc
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-15 23:04:59 -0500

    Create a Contacts class that has a name and phone number

---
+++ Step8_Arrays/MobilePhoneChallenge/src/Contacts.java
@@ -0,0 +1,9 @@
+public class Contacts {
+    private String name;
+    private String phoneNumber;
+
+    public Contacts(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+}
