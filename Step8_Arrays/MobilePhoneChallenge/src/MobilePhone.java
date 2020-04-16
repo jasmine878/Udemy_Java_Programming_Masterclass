3c2f60521f182055b4d733ff6576560e280a0d92
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-15 23:58:16 -0500

    Create methods to be able to add a contact to a MobilePhone object

--- Step8_Arrays/MobilePhoneChallenge/src/MobilePhone.java
+++ Step8_Arrays/MobilePhoneChallenge/src/MobilePhone.java
@@ -9,4 +9,28 @@ public class MobilePhone {
         this.myNumber = myNumber;
         this.myContacts = new ArrayList<Contact>();
     }
+
+    public boolean addContact(Contact newContact) {
+        if (findContact(newContact.getName()) >= 0) {
+            System.out.println("Contact already exists");
+            return false;
+        } else {
+            myContacts.add(newContact);
+            return true;
+        }
+    }
+
+    private int findContact(String name) {
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+
+            if (contact.getName().equals(name)) return i;
+        }
+
+        return -1;
+    }
+
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact;
+    }
 }
