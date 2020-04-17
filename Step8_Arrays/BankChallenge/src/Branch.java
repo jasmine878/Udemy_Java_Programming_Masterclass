4895369c28971637c12e365b21efd28af13fcb1c
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-17 15:58:40 -0500

    Create a doesCustomerExist helper method and refactor code

--- Step8_Arrays/BankChallenge/src/Branch.java
+++ Step8_Arrays/BankChallenge/src/Branch.java
@@ -12,27 +12,36 @@ public class Branch {
         return this.customers;
     }
 
+    public boolean doesCustomerExist(String name) {
+        for (int i = 0; i < getCustomers().size(); i++) {
+            if (getCustomers().get(i).getName().equals(name)) return true
+        }
+        return false;
+    }
+
     public void addCustomer(String name, double initialTransaction) {
-        ArrayList<Double> newArrayList = new ArrayList<Double>();
-        newArrayList.add(initialTransaction);
+        if (doesCustomerExist(name)) {
+            System.out.println("Cannot add new customer.  Name already exists.");
+        } else {
+            ArrayList<Double> newArrayList = new ArrayList<Double>();
+            newArrayList.add(initialTransaction);
 
-        Customer newCustomer = new Customer(name, newArrayList);
+            Customer newCustomer = new Customer(name, newArrayList);
 
-        getCustomers().add(newCustomer);
+            getCustomers().add(newCustomer);
+            System.out.println("Successfuly added new customer");
+        }
     }
 
     public void addTransactions(String name, double nextTransaction) {
-        boolean foundCustomer = false;
-
-        for (int i = 0; i < getCustomers().size(); i++) {
-            if (getCustomers().get(i).getName().equals(name)) {
-                getCustomers().get(i).getTransactions().add(nextTransaction);
-                foundCustomer = true;
+        if (doesCustomerExist(name)) {
+            for (int i = 0; i < getCustomers().size(); i++) {
+                if (getCustomers().get(i).getName().equals(name)) {
+                    getCustomers().get(i).getTransactions().add(nextTransaction);
+                    System.out.println("Sucessfully added transaction to customer");
+                }
             }
-        }
-
-        if (foundCustomer) System.out.println("Sucessfully added transaction to customer");
-        else System.out.println("Cannot find the customer");
+        } else System.out.println("Cannot find the customer.  Unable to add transaction.");
     }
 
     public void getCustomerList() {
