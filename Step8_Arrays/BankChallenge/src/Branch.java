c58cd2759f2aa37190239e08a5d144893562f5d4
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-17 17:31:25 -0500

    Change logic to add a new customer to a Branch

--- Step8_Arrays/BankChallenge/src/Branch.java
+++ Step8_Arrays/BankChallenge/src/Branch.java
@@ -13,42 +13,36 @@ public class Branch {
         return name;
     }
 
-    //    public ArrayList<Customer> getCustomers() {
-//        return this.customers;
-//    }
-//
-//    public boolean doesCustomerExist(String name) {
-//        for (int i = 0; i < getCustomers().size(); i++) {
-//            if (getCustomers().get(i).getName().equals(name)) return true
-//        }
-//        return false;
-//    }
-//
-//    public void addCustomer(String name, double initialTransaction) {
-//        if (doesCustomerExist(name)) {
-//            System.out.println("Cannot add new customer.  Name already exists.");
-//        } else {
-//            ArrayList<Double> newArrayList = new ArrayList<Double>();
-//            newArrayList.add(initialTransaction);
-//
-//            Customer newCustomer = new Customer(name, newArrayList);
-//
-//            getCustomers().add(newCustomer);
-//            System.out.println("Successfuly added new customer");
-//        }
-//    }
-//
-//    public void addTransactions(String name, double nextTransaction) {
-//        if (doesCustomerExist(name)) {
-//            for (int i = 0; i < getCustomers().size(); i++) {
-//                if (getCustomers().get(i).getName().equals(name)) {
-//                    getCustomers().get(i).getTransactions().add(nextTransaction);
-//                    System.out.println("Sucessfully added transaction to customer");
-//                }
-//            }
-//        } else System.out.println("Cannot find the customer.  Unable to add transaction");
-//    }
-//
+    public ArrayList<Customer> getCustomers() {
+        return this.customers;
+    }
+
+    public boolean doesCustomerExist(String name) {
+        for (int i = 0; i < getCustomers().size(); i++) {
+            if (getCustomers().get(i).getName().equals(name)) return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomer(String name, double initialTransaction) {
+        if (doesCustomerExist(name)) return false;
+        else {
+            this.customers.add(new Customer(name, initialTransaction));
+            return true;
+        }
+    }
+
+    public void addTransactions(String name, double nextTransaction) {
+        if (doesCustomerExist(name)) {
+            for (int i = 0; i < getCustomers().size(); i++) {
+                if (getCustomers().get(i).getName().equals(name)) {
+                    getCustomers().get(i).getTransactions().add(nextTransaction);
+                    System.out.println("Sucessfully added transaction to customer");
+                }
+            }
+        } else System.out.println("Cannot find the customer.  Unable to add transaction");
+    }
+
 //    public void getCustomerList() {
 //        for (int i = 0; i < getCustomers().size(); i++) {
 //            System.out.println(i + " " + getCustomers().get(i).getName());
