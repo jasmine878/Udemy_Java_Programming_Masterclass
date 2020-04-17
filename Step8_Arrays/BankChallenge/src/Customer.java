81271e1731815006ec99d58ea1face1840028081
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-17 15:01:11 -0500

    Create a Customer Class

--- Step8_Arrays/BankChallenge/src/Customer.java
+++ Step8_Arrays/BankChallenge/src/Customer.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Customer {
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, ArrayList<Double> transactions) {
+        this.name = name;
+        this.transactions = transactions;
+    }
+
+    public String getName() {
+        return this.getName();
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return this.getTransactions();
+    }
 }
