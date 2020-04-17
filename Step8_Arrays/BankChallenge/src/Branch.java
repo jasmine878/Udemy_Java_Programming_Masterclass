04264860911f76a625ba783781dcc870816acdf2
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-17 14:57:12 -0500

    Create a Branch Class

--- Step8_Arrays/BankChallenge/src/Branch.java
+++ Step8_Arrays/BankChallenge/src/Branch.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Branch {
+    private ArrayList<Customer> customers = new ArrayList<Customer>();
+
+    public Branch(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return this.customers;
+    }
 }
