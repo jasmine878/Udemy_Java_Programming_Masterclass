226f4bfe7fe8a0ffcabdee6fdf590c5e9f636b41
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-16 23:40:38 -0500

    Code an Autoboxing and unboxing example for ArrayLists using Double values

--- Step8_Arrays/Autoboxing/src/Main.java
+++ Step8_Arrays/Autoboxing/src/Main.java
@@ -16,7 +16,18 @@ public class Main {
         for (int i = 0; i <= 10; i++) {
             System.out.println(i + " --> " + intArrayList.get(i).intValue());
         }
+        System.out.println();
 
+        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+
+        for (double dbl = 0.0; dbl <=10.0; dbl+= 0.5) {
+            myDoubleValues.add(Double.valueOf(dbl));
+        }
+
+        for (int i = 0; i < myDoubleValues.size(); i ++) {
+            double value = myDoubleValues.get(i).doubleValue();
+            System.out.println(i + " --> " + value);
+        }
     }
 }
 
