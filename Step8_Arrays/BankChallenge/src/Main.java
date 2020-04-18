a81b3fe945765732875ff08556630c4f06c44154
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-17 20:43:42 -0500

    Create a few examples in the Main Class to run

--- Step8_Arrays/BankChallenge/src/Main.java
+++ Step8_Arrays/BankChallenge/src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank("National Australia Bank");
 
-        bank.addBranch("Adelaide");
+        if(bank.addBranch("Adelaide")) {
+            System.out.println("Adelaide branch created");
+        };
+        System.out.println();
 
         bank.addNewCustomer("Adelaide", "Tim", 50.05);
         bank.addNewCustomer("Adelaide", "Mike", 175.34);
@@ -16,6 +19,25 @@ public class Main {
         bank.addCustomerTransaction("Adelaide", "Mike", 1.65);
 
         bank.getCustomerList("Adelaide", true);
+        bank.getCustomerList("Sydney", true);
+        bank.addBranch("Melbourne");
+        System.out.println();
+
+        if (!bank.addNewCustomer("Melbourne", "Brian", 5.53)) {
+            System.out.println("Error Melbourne branch does not exist");
+        }
+
+        if (!bank.addBranch("Adelaide")) {
+            System.out.println("Adelaide branch already exists");
+        }
+
+        if(!bank.addCustomerTransaction("Adelaide", "Fergus", 52.33)) {
+            System.out.println("Customer does not exist at branch");
+        }
+
+        if(!bank.addNewCustomer("Adelaide", "Tim", 12.21)) {
+            System.out.println("Customer Tim already exists");
+        }
     }
 
 }
