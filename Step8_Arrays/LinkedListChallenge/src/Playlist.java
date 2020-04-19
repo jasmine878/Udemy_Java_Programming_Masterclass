ee072bf5317633720fe675b9fea5a8c7631d7c9d
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-18 19:55:57 -0500

    Create a Playlist Class

--- Step8_Arrays/LinkedListChallenge/src/Playlist.java
+++ Step8_Arrays/LinkedListChallenge/src/Playlist.java
@@ -1,3 +1,21 @@
-public class Playlist {
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+public class Playlist {
+    private ArrayList<Album> albums;
+    private LinkedList<Song> playlist;
+
+    public Playlist(ArrayList<Album> albums, LinkedList<Song> playlist) {
+        this.albums = albums;
+        this.playlist = playlist;
+    }
+
+    public ArrayList<Album> getAlbums() {
+        return this.albums;
+    }
+
+    public LinkedList<Song> getPlaylist() {
+        return this.playlist;
+    }
+    
 }
