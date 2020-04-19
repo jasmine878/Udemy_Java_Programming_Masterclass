334e9739415e8d2c7f8d4215e2bf0cdc01ffefdc
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-18 21:00:05 -0500

    Create a menuOfOptions method in the Playlist Class

--- Step8_Arrays/LinkedListChallenge/src/Playlist.java
+++ Step8_Arrays/LinkedListChallenge/src/Playlist.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Playlist {
     private LinkedList<Song> playlist;
@@ -22,4 +24,30 @@ public class Playlist {
             playlist.add(song);
         }
     }
+
+    public static void menuOfOptions(LinkedList playlist) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+
+        ListIterator<Song> playlistTool = playlist.listIterator();
+
+        if (playlist.isEmpty()) {
+            System.out.println("There are no songs in the playlist");
+        } else {
+            System.out.println("Song playing from Playlist is: " + playlistTool.next());
+            printMenu();
+        }
+        
+    }
+
+    private static void printMenu() {
+        System.out.println("Available actions: \npress ");
+        System.out.println("0 - Quit\n" +
+                            "1 - Skip forward to next song\n" +
+                            "2 - Skip backwards to previous song\n" +
+                            "3 - Replay the current song\n" +
+                            "4 - List playlist songs");
+    }
+
 }
