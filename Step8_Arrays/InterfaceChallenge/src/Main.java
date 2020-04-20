83e7d06cf2210e4a5af864ef6abdcd04cbcc56ab
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-20 13:44:59 -0500

    Copy the readValues method from Instructor's resource file into the Main Class

---
+++ Step8_Arrays/InterfaceChallenge/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    //this method simulates getting values from a file
+    //since we haven't learned Java Input/Output yet
+    //this allows you to type in as many values as the Class requires and returns an ArrayList
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+}
