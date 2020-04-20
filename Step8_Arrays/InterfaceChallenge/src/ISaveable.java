4389c5deb06f5d624e4168a783ee1a0c38105359
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-20 14:09:24 -0500

    Create an ISaveable Interface

--- Step8_Arrays/InterfaceChallenge/src/ISaveable.java
+++ Step8_Arrays/InterfaceChallenge/src/ISaveable.java
@@ -1,3 +1,15 @@
+// Create a simple interface (tip: name it Saveable or ISaveable)
+// That interface allows an object to be saved to some sort of storage medium. (tip: use arraylist)
+// The exact type of medium is not known to the interface (nor to the classes that implement it).
+// The interface will just specify two methods,
+
+import java.util.ArrayList;
+
 public interface ISaveable {
 
+    // one to return an ArrayList of values to be saved and the other
+    ArrayList<String> write();
+
+    // to populate the object's fields from an ArrayList (parameter).
+    void read(ArrayList<String> savedValues);
 }
