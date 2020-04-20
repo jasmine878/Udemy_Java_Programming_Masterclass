681b8eb1e8bef9fa4276e7aff6dbd2d1ef6563e7
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-20 14:20:40 -0500

    Create a Player Class that implements the ISaveable Interface

--- Step8_Arrays/InterfaceChallenge/src/Player.java
+++ Step8_Arrays/InterfaceChallenge/src/Player.java
@@ -1,1 +1,58 @@
+import java.util.ArrayList;
 
+public class Player implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
+    private String weapon;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    @Override
+    public ArrayList<String> write() {
+        return null;
+    }
+
+    @Override
+    public void read(ArrayList<String> savedValues) {
+
+    }
+    
+}
