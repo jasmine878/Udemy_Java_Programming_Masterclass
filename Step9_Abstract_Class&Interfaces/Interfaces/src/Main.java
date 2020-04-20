6985966fb400a23ea4e58c139b30091f4aee6dfc
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-19 23:17:04 -0500

    Add another example in the Main method

--- Step9_Abstract_Class&Interfaces/Interfaces/src/Main.java
+++ Step9_Abstract_Class&Interfaces/Interfaces/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        ITelephone timsPhone;
-        timsPhone = new DeskPhone(123456);
+        ITelephone timsPhone = new DeskPhone(123456);
+
+        timsPhone.powerOn();
+        timsPhone.callPhone(555-555-5555);
+        timsPhone.answer();
+
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();;
     }
 }
