d80dced42a6fa739bd965f28cd14bd7f4249242a
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-20 23:54:26 -0500

    Create a Gearbox class

---
+++ Step9_Abstract_Class&Interfaces/InnerClasses/src/Gearbox.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Gearbox {
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int gearNumber = 0;
+
+    public Gearbox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<Gear>();
+
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+    }
+}
