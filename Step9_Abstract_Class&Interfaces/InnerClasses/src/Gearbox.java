bcc743ede15f73705739f277174e7452b75793aa
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-21 11:40:34 -0500

    Make the inner class private and add methods to the outer class

--- Step9_Abstract_Class&Interfaces/InnerClasses/src/Gearbox.java
+++ Step9_Abstract_Class&Interfaces/InnerClasses/src/Gearbox.java
@@ -4,6 +4,7 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -13,16 +14,49 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
+    public void operateClutch(boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio) {
+        if (number > 0 && number <= maxGears) {
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear) {
+        if (newGear >= 0 && newGear < this.gears.size() && this.clutchIsIn) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected");
+        } else {
+            System.out.println("Grind!");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs) {
+        if(clutchIsIn) {
+            System.out.println("Scream!!!");
+            return 0.0;
+        }
+
+        return revs * gears.get(currentGear).getRatio();
+    }
+
     //we created an inner Class here because we don't need a gear unless we refer to a Gearbox
     //a Gear isn't useful in its own right.  It needs to be coupled with a Gearbox to be useful
     //it doesn't make sense to talk about Gears, unless we talk about a Gearbox
-    public class Gear {
+    private class Gear {
         private int gearNumber;
         private double ratio;
 
         public Gear(int gearNumber, double ratio) {
             this.gearNumber = gearNumber;
             this.ratio = ratio;
+        }
+
+        public double getRatio() {
+            return ratio;
         }
 
         public double driveSpeed(int revs) {
