1d292472eb68f709cd65e57609a81dd176e4bf6e
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-21 12:22:02 -0500

    Remove the addGear method from the Main class and run it

--- Step9_Abstract_Class&Interfaces/InnerClasses/src/Main.java
+++ Step9_Abstract_Class&Interfaces/InnerClasses/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 //        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
 //        System.out.println(first.driveSpeed(1000));
 
-        mcLaren.addGear(1, 5.3);
-        mcLaren.addGear(2, 10.6);
-        mcLaren.addGear(3, 15.9);
+//        mcLaren.addGear(1, 5.3);                      //equals 5.3 * 1
+//        mcLaren.addGear(2, 10.6);                     //equals 5.3 * 2
+//        mcLaren.addGear(3, 15.9);                     //equals 5.3 * 3
         mcLaren.operateClutch(true);
         mcLaren.changeGear(1);
         mcLaren.operateClutch(false);
