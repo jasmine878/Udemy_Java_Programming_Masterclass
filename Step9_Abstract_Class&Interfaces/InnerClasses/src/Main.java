33538093dc55ba016587343d2f15e5c4a9cf369a
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-21 11:53:26 -0500

    Remove all references of the inner class from the Main Class

--- Step9_Abstract_Class&Interfaces/InnerClasses/src/Main.java
+++ Step9_Abstract_Class&Interfaces/InnerClasses/src/Main.java
@@ -1,8 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
-        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+//        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+//        System.out.println(first.driveSpeed(1000));
 
-        System.out.println(first.driveSpeed(1000));
+        mcLaren.addGear(1, 5.3);
+        mcLaren.addGear(2, 10.6);
+        mcLaren.addGear(3, 15.9);
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        System.out.println();
+
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        System.out.println();
+
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
     }
 }
