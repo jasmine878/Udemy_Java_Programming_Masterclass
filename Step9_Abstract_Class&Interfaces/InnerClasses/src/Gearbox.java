60f9642936b7e8c3562109d2835e82cd4bafa245
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-21 00:10:06 -0500

    Create an inner Gear class inside the Gearbox class

--- Step9_Abstract_Class&Interfaces/InnerClasses/src/Gearbox.java
+++ Step9_Abstract_Class&Interfaces/InnerClasses/src/Gearbox.java
@@ -12,4 +12,21 @@ public class Gearbox {
         Gear neutral = new Gear(0, 0.0);
         this.gears.add(neutral);
     }
+
+    //we created an inner Class here because we don't need a gear unless we refer to a Gearbox
+    //a Gear isn't useful in its own right.  It needs to be coupled with a Gearbox to be useful
+    //it doesn't make sense to talk about Gears, unless we talk about a Gearbox
+    public class Gear {
+        private int gearNumber;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
+
+        public double driveSpeed(int revs) {
+            return revs * this.ratio;
+        }
+    }
 }
