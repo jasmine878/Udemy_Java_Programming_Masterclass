1f960821c9fce126949efb785c0e463838a383ac
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-22 18:08:54 -0500

    Create abstract methods in the abstract class ListItem

--- Step9_Abstract_Class&Interfaces/AbstractClassesChallenge/src/ListItem.java
+++ Step9_Abstract_Class&Interfaces/AbstractClassesChallenge/src/ListItem.java
@@ -1,10 +1,28 @@
 public abstract class ListItem {
+    //we're using protected, because we need to be able to access these variables from our subclass
     protected ListItem next = null;
     protected ListItem previous = null;
     protected Object value;
 
+    //constructor
     public ListItem(Object value) {
         this.value = value;
     }
-    
+
+    //getter
+    public Object getValue() {
+        return value;
+    }
+
+    //setter
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    //abstract methods
+    abstract ListItem next();
+    abstract ListItem previous();
+    abstract void setNext(ListItem nextItem);
+    abstract void setPrevious(ListItem previousItem);
+    abstract int compareTo(ListItem compareItem);
 }
