ae6d4229986074e23a77c5db5f777bf6ef0d8322
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-23 17:37:55 -0500

    create a myLinkedList object and remove items in the Linked List

--- Step9_Abstract_Class&Interfaces/AbstractClassesChallenge/src/Main.java
+++ Step9_Abstract_Class&Interfaces/AbstractClassesChallenge/src/Main.java
@@ -14,8 +14,32 @@ public class Main {
         list.traverse(list.getRoot());
         System.out.println();
 
-      
+        MyLinkedList list2 = new MyLinkedList();
+        String stringData2 = "5 7 3 9 8 2 1 0 4 6";
+        String[] dataArray2 = stringData2.split(" ");
+
+        for (String word : dataArray2) {
+            list2.addItem(new Node(word));
+        }
+
+        list2.traverse(list2.getRoot());
+        System.out.println();
+        list2.removeItem(new Node("5"));
+        list2.traverse(list2.getRoot());
+        list2.removeItem(new Node("3"));
+        list2.traverse(list2.getRoot());
+        list2.removeItem(new Node("20"));
+        list2.traverse(list2.getRoot());
+        list2.removeItem(new Node("0"));
+        list2.removeItem(new Node("1"));
+        list2.removeItem(new Node("2"));
+        list2.removeItem(new Node("6"));
+        list2.removeItem(new Node("7"));
+        list2.removeItem(new Node("8"));
+        list2.traverse(list2.getRoot());
+        list2.removeItem(list2.getRoot());
+        list2.traverse(list2.getRoot());
+        list2.removeItem(list2.getRoot());
+        list2.traverse(list2.getRoot());
     }
-
-
 }
