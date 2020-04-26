00f4f377021075945ad9dfdbbf4d015b833a9147
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-26 00:19:00 -0500

    Create a footballLeague instance and add football teams to the instance

--- Step9_Abstract_Class&Interfaces/GenericsChallenge/src/Main.java
+++ Step9_Abstract_Class&Interfaces/GenericsChallenge/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        //ArrayList<Team> teams;
-        //Collections.sort(teams);
+       League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
+
+        Team<FootballPlayer> adelaideCrows2 = new Team<>("Adelaide Crows");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        footballLeague.addTeam(adelaideCrows2);
+        footballLeague.addTeam(melbourne);
+        footballLeague.addTeam(hawthorn);
+        footballLeague.addTeam(fremantle);
+
+
     }
 }
