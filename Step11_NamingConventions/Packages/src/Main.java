2e1ed49a75dcfad3890d46460ae76b98e0c8097d
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-26 14:22:02 -0500

    Utilize a package for the Node Class

---
+++ Step11_NamingConventions/Packages/src/Main.java
@@ -0,0 +1,13 @@
+//import javafx.scene.Node;
+
+public class Main {
+    public static void main(String[] args) {
+//        Node node = null;
+
+        //we're specifying the package that contains the Node class we want to use here
+        //in this case the package is not imported
+        //as a result, everytime we want to reference this Node class we have to type out the package as well
+        javafx.scene.Node node = null;
+    }
+
+}
