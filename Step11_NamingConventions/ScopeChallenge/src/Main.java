a84ac500e8509580fed0fe2250cb56b6f7e387b7
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-27 16:04:30 -0500

    Create a scanner instance to read an input number for our parameter in the timesTable method

--- Step11_NamingConventions/ScopeChallenge/src/Main.java
+++ Step11_NamingConventions/ScopeChallenge/src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         RegularVariables regularVariables = new RegularVariables();
-        regularVariables.timesTable(6);
+
+        System.out.println("Enter a number for the timesTable method");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+
+        regularVariables.timesTable(number);
     }
 }
