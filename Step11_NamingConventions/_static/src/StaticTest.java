f85150df012f269933840c9cb0601e0c398335bb
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-27 18:05:23 -0500

    Create a StaticTest Class

---
+++ Step11_NamingConventions/_static/src/StaticTest.java
@@ -0,0 +1,17 @@
+public class StaticTest {
+    private int numInstances = 0;
+    private String name;
+
+    public StaticTest(String name) {
+        this.name = name;
+        numInstances++;
+    }
+
+    public int getNumInstances() {
+        return this.numInstances;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
