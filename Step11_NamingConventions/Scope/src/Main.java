be27903cc4c2dfc09acd9bbe9abca8438921986d
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-27 10:08:53 -0500

    Rename same name variables for easier differentiation between scopes

--- Step11_NamingConventions/Scope/src/Main.java
+++ Step11_NamingConventions/Scope/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        String privateVar = "this is private to main()";
+        String var4 = "this is private to main()";
         ScopeCheck scopeInstance = new ScopeCheck();
 
-        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        System.out.println("scopeInstance Var1 is " + scopeInstance.getVar1());
+        System.out.println(var4);
 
         scopeInstance.timesTwo();
         System.out.println("******************");
@@ -14,3 +14,4 @@ public class Main {
         innerClass.timesTwo();
     }
 }
+
--- Step11_NamingConventions/Scope/src/ScopeCheck.java
+++ Step11_NamingConventions/Scope/src/ScopeCheck.java
@@ -1,38 +1,39 @@
 
 public class ScopeCheck {
     public int publicVar = 0;
-    private int privateVar = 1;
+    private int var1 = 1;
 
     public ScopeCheck() {
-        System.out.println("ScopeCheck created, publicVar = " + publicVar + ": privateVar = " + privateVar);
+        System.out.println("ScopeCheck created, publicVar = " + publicVar + ": privateVar = " + var1);
     }
 
-    public int getPrivateVar() {
-        return privateVar;
+    public int getVar1() {
+        return var1;
     }
 
     public void timesTwo() {
-        int privateVar = 2;
+        int var2 = 2;
 
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + " times two is " + i * privateVar);
+            System.out.println(i + " times two is " + i * var2);
         }
         System.out.println("============================");
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + " times two is " + i * this.privateVar);
+            System.out.println(i + " times two is " + i * this.var1);
         }
     }
 
     public class InnerClass {
-        public int privateVar = 3;
+        public int var3 = 3;
 
         public InnerClass() {
-            System.out.println("InnerClass created, privateVar is " + privateVar);
+            System.out.println("InnerClass created, var1 is " + var1 + " and var3 is " + var3);
         }
 
         public void timesTwo() {
+            System.out.println("varOne is still available here " + var1);
             for (int i = 0; i < 10; i++) {
-                System.out.println(i + " times two is " + i * privateVar);
+                System.out.println(i + " times two is " + i * var3);
 //                We can also use the privateVar from the Outer Class as well
 //                System.out.println(i + " times two is " + i * ScopeCheck.this.privateVar);
             }
