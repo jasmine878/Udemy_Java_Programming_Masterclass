92b96458e0b8cda6a3e6bc5babb289f5b96feb17
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-27 17:05:32 -0500

    Create deposit and withdrawal methods in the Account Class

--- Step11_NamingConventions/AccessModifiers/src/Account.java
+++ Step11_NamingConventions/AccessModifiers/src/Account.java
@@ -13,4 +13,26 @@ public class Account {
     public int getBalance() {
         return this.balance;
     }
+
+    public void deposit(int amount) {
+        if (amount > 0) {
+            transactions.add(amount);
+            this.balance += amount;
+            System.out.println(amount + " deposited.  Balance is now " + this.balance);
+        } else {
+            System.out.println("Cannot deposit negative sums");
+        }
+    }
+
+    public void withdrawal(int amount) {
+        int withdrawal = -amount;
+
+        if (withdrawal < 0) {
+            transactions.add(withdrawal);
+            this.balance += withdrawal;
+            System.out.println(amount + " withdrawn.  Balance is now " + this.balance);
+        } else {
+            System.out.println("Cannot withdraw negative sums");
+        }
+    }
 }
