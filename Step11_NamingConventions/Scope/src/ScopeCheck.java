1ce71e3003340db1cea2be1e4c932ed3a97b0ccf
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-26 23:59:03 -0500

    Create an Inner Class with the same name variable for testing purposes

--- Step11_NamingConventions/Scope/src/ScopeCheck.java
+++ Step11_NamingConventions/Scope/src/ScopeCheck.java
@@ -22,4 +22,20 @@ public class ScopeCheck {
             System.out.println(i + " times two is " + i * this.privateVar);
         }
     }
+
+    public class InnerClass {
+        public int privateVar = 3;
+
+        public InnerClass() {
+            System.out.println("InnerClass created, privateVar is " + privateVar);
+        }
+
+        public void timesTwo() {
+            int privateVar = 2;
+
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i + " times two is " + i * privateVar);
+            }
+        }
+    }
 }
