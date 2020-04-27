ab24ab5b2c7474a70afe5404924c4bab16a41050
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-27 16:20:51 -0500

    Rename all variables to x

--- Step11_NamingConventions/ScopeChallenge/src/Main.java
+++ Step11_NamingConventions/ScopeChallenge/src/Main.java
@@ -2,13 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        RegularVariables regularVariables = new RegularVariables();
+//        Scanner scanner = new Scanner(System.in);
+//        RegularVariables regularVariables = new RegularVariables();
+//
+//        System.out.println("Enter a number for the timesTable method");
+//        int number = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        regularVariables.timesTable(number);
+//        scanner.close();
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number for the timesTable method");
         int number = scanner.nextInt();
         scanner.nextLine();
+        X x = new X(number);
 
-        regularVariables.timesTable(number);
+        x.x();
+        scanner.close();
+
     }
 }
---
+++ Step11_NamingConventions/ScopeChallenge/src/X.java
@@ -0,0 +1,19 @@
+//converting Regular Variables class to use 'x' for all variable, method, and class names
+//we want to see scope in play with same name variables and instances
+
+public class X {
+    private int x;
+
+    public X(int x) {
+        this.x = x;
+    }
+
+    public void x() {
+        System.out.println("Times table for " + x + ": ");
+        for (int x = 1;x <= 12; x++) {
+            int result = this.x * x;
+            System.out.println(this.x + " multiplied by " + x + " equals " + result);
+        }
+        System.out.println("===============================");
+    }
+}
