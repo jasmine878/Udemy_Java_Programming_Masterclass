c0e012db70dd0c4b1a1bb11ae3858055f3f8e01a
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-27 17:17:06 -0500

    Create an Account instance and invoke various methods from the Account Class

--- Step11_NamingConventions/AccessModifiers/src/Main.java
+++ Step11_NamingConventions/AccessModifiers/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Account timsAccount = new Account("Tim");
+
+        timsAccount.deposit(1000);
+        timsAccount.withdrawal(500);
+        timsAccount.withdrawal(-200);
+        timsAccount.deposit(-20);
+        timsAccount.calculateBalance();
+
+        System.out.println("Balance on account is " + timsAccount.getBalance());
 
     }
 }
