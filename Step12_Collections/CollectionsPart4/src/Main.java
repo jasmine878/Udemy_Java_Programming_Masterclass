8b5cac63ae4a7ee29b03971203273b494dd1db93
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-28 22:44:55 -0500

    Make a reservation in our copy arrayList and verify its been changed in the original arrayList as well

--- Step12_Collections/CollectionsPart4/src/Main.java
+++ Step12_Collections/CollectionsPart4/src/Main.java
@@ -9,6 +9,14 @@ public class Main {
         List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
         printList(seatCopy);
 
+        //make a reservation of the 2nd seat from our arraylist copy
+        //note the reservation is also made in the original theatre arrayList as well
+        seatCopy.get(1).reserve();
+        if (theatre.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
+        } else {
+            System.out.println("Seat already reserved");
+        }
     }
 
     //method that prints the List
