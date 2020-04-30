803c66f73a9a21987aeadc41bdd9aa4290873138
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-29 23:46:20 -0500

    print the return value for the put method for various key-value pairs

--- Step12_Collections/CollectionsPart6/src/MapProgram.java
+++ Step12_Collections/CollectionsPart6/src/MapProgram.java
@@ -11,11 +11,14 @@ public class MapProgram {
         languages.put("Java", "a compiled high level, object-oriented, platform independent language");
         languages.put("Python", "an interpreted, object-oriented, high-level programming language with dynamic semantics");
         languages.put("Algol", "algorithmic language");
-        languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code");
-        languages.put("Lisp", "Therein lies madness");
 
         System.out.println(languages.get("Java"));
         languages.put("Java", "this course is about Java");
         System.out.println(languages.get("Java"));
+
+        System.out.println(languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code"));
+        System.out.println(languages.put("Lisp", "Therein lies madness"));
+        System.out.println(languages.put("Python", "this is a test"));
+
     }
 }
