48531d6ec8c9037d2bd5e2acc4240c281753d482
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-30 21:44:27 -0500

    Add comments in the Main Class

--- Step12_Collections/AdventureGame/src/Main.java
+++ Step12_Collections/AdventureGame/src/Main.java
@@ -3,11 +3,13 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    //our hashMap has an Integer for the key and the Class Location for our values
     private static Map<Integer, Location> locations = new HashMap<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //the primitive int type gets converted to Integers through auto-boxing!
         locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
         locations.put(1, new Location(1,"You are standing at the end of a road before a small brick building"));
         locations.put(2, new Location(2, "You are at the top of a hill"));
