b88ccc93326477608d6b25d989ef4724568709eb
Tae Hui Chang <jasmine878@yahoo.com>
2020-04-30 19:53:12 -0500

    Test the replace method using 3 parameters

--- Step12_Collections/CollectionsPart7/src/MapProgram.java
+++ Step12_Collections/CollectionsPart7/src/MapProgram.java
@@ -56,7 +56,11 @@ public class MapProgram {
         System.out.println(languages.replace("Lisp", "a functional programming language with imperative features"));
         System.out.println(languages.replace("Scala", "this will not be added"));
 
-
+        if (languages.replace("Lisp", "This will not work", "another definition")) {
+            System.out.println("Lisp replaced");
+        } else {
+            System.out.println("Lisp was not replaced");
+        }
 
         for (String key: languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
