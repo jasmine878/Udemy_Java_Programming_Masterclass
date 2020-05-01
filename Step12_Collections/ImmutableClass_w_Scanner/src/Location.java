364529a7e0e9f7e2186005c3b0571b6f81f9060d
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-01 15:19:21 -0500

    Assign the exits Map in the constructor

--- Step12_Collections/ImmutableClass_w_Scanner/src/Location.java
+++ Step12_Collections/ImmutableClass_w_Scanner/src/Location.java
@@ -6,10 +6,11 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>();
+        this.exits = exits;
+
         this.exits.put("Q", 0);
     }
 
@@ -25,7 +26,8 @@ public class Location {
         return new HashMap<String, Integer>(exits);         //returns a copy of the exits HashMap
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+    //remove since exits are now included in the constructor
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 }
