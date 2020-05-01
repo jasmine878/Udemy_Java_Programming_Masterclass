d42eeb724f20fdfca6b8111fada19855847ad6fd
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-01 18:57:46 -0500

    Create a HeavenlyBody Class

---
+++ Step12_Collections/SetAndHashSet/src/HeavenlyBody.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public final class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return this.getName();
+    }
+
+    public double getOrbitalPeriod() {
+        return this.orbitalPeriod;
+    }
+}
