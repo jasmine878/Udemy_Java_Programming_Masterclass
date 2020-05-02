c1c98a7c216cf7602e00c8dceb7f862ef86b7933
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-01 23:39:30 -0500

    Create a HashMap and a HashSet in the Main Class

--- Step12_Collections/SetAndHashSet/src/Main.java
+++ Step12_Collections/SetAndHashSet/src/Main.java
@@ -1,4 +1,12 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
     public static void main(String[] args) {
 
     }
