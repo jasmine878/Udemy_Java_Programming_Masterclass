b1d25dade445a0d8a31c1e1b497bc1f478bcb11f
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-02 21:42:05 -0500

    Create an intersection of the two hashSets

--- Step12_Collections/SetAndHashSetPart3/src/Asymmetric.java
+++ Step12_Collections/SetAndHashSetPart3/src/Asymmetric.java
@@ -15,8 +15,20 @@ public class Asymmetric {
         System.out.println();
 
         //These bulk operations are destructive which means they modify the set they're called upon
+        //this is why we create a 3rd hashSet
         Set<Integer> squaresAndCubes = new HashSet<>(squares);
         squaresAndCubes.addAll(cubes);
         System.out.println("A union of the hashSets results in " + squaresAndCubes.size() + " elements.");
+
+        //our union of the hashSets only prints a size of 196 elements
+        //Since we're missing 4 elements, they must be in the intersection
+        Set<Integer> intersection = new HashSet<>(squares);
+        intersection.retainAll(cubes);
+        System.out.println("Intersection contains " + intersection.size() + " elements.");;
+        System.out.println();
+
+        for (int i : intersection) {
+            System.out.println(i + " is the square of " + Math.sqrt(i) + " and the cube of " + Math.cbrt(i));
+        }
     }
 }
