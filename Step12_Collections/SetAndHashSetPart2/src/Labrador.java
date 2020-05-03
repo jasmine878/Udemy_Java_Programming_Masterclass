886ecc1401e4b2cbbc4295ee4903b4de65b6081b
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-02 19:02:29 -0500

    Mark the Dog equals method as final and remove the Labrador equals method to fix and make the instances equal

--- Step12_Collections/SetAndHashSetPart2/src/Labrador.java
+++ Step12_Collections/SetAndHashSetPart2/src/Labrador.java
@@ -3,16 +3,16 @@ public class Labrador extends Dog{
         super(name);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-
-        if (obj instanceof Labrador) {
-            String objName = ((Labrador) obj).getName();
-
-            return this.getName().equals(objName);
-        }
-
-        return false;
-    }
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) return true;
+//
+//        if (obj instanceof Labrador) {
+//            String objName = ((Labrador) obj).getName();
+//
+//            return this.getName().equals(objName);
+//        }
+//
+//        return false;
+//    }
 }
