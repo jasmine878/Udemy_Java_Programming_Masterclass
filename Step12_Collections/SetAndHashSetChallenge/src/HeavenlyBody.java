ef79aef0fea697a4d8ab792d639d3322c1c653d2
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-03 16:38:52 -0500

    Use an enum to create the types of BodyTypes in our HeavenlyBody Class

--- Step12_Collections/SetAndHashSetChallenge/src/HeavenlyBody.java
+++ Step12_Collections/SetAndHashSetChallenge/src/HeavenlyBody.java
@@ -7,11 +7,35 @@ public final class HeavenlyBody {
     private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
+//    private final int bodyType;
+    private final BodyTypes bodyType;
 
-    public HeavenlyBody(String name, double orbitalPeriod) {
+    //defining the various bodyTypes as constants
+    //using separate declarations
+//    public static final int STAR = 1;
+//    public static final int PLANET = 2;
+//    public static final int DWARF_PLANET = 3;
+//    public static final int MOON = 4;
+//    public static final int COMET = 5;
+//    public static final int ASTEROID = 6;
+
+    //ENUM - Java's way of grouping constants together
+    //enums are types
+    public enum BodyTypes {
+        STAR,
+        PLANET,
+        DWARF_PLANET,
+        MOON,
+        COMET,
+        ASTEROID
+    }
+
+    //an enum will only accept a parameter that is in its acceptable list
+    public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
         this.name = name;
         this.orbitalPeriod = orbitalPeriod;
         this.satellites = new HashSet<>();
+        this.bodyType = bodyType;
     }
 
     public String getName() {
