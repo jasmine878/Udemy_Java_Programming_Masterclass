9ecaf0ab15b3a29b4824986e1834b90e5b56311e
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-04 09:07:00 -0500

    Create a StockItem Class

---
+++ Step12_Collections/Sorted Collections/src/StockItem.java
@@ -0,0 +1,40 @@
+//We need to implement the Comparable class because we're creating a Linked HashMap which is sorted
+public class StockItem implements Comparable<StockItem>{
+    private final String name;
+    private double price;
+    private int quantity;
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantity = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0.0) {
+            this.price = price;
+        }
+    }
+
+    public void adjustQuantity(int quantity) {
+        //quantity can be positive or negative
+        //we don't want to save the adjustment unless our quantity remains positive
+        int newQuantity = this.quantity + quantity;
+
+        if (newQuantity >= 0) {
+            this.quantity = newQuantity;
+        }
+    }
+}
