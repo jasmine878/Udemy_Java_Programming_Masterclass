a676fc3080b0988249f0c35d0caea23e63931f93
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-04 15:33:36 -0500

    Create an addStock method that adds to our HashMap and returns and updated qty

--- Step12_Collections/Sorted Collections/src/StockList.java
+++ Step12_Collections/Sorted Collections/src/StockList.java
@@ -10,4 +10,27 @@ public class StockList {
     public StockList() {
         list = new HashMap<>();
     }
+
+    public int addStock(StockItem item) {
+        if (item != null) {
+            //we're using the item name for our key in our map
+            //if we find an existing item in our list assign it to inStock
+            //if not, assign inStock to our parameter item which is our default
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+
+            //if the item exists add our existing quantity to our item qty
+            if (inStock != item) item.adjustQuantity(inStock.getQuantity());
+
+            //we're adding the item to our inventory
+            //with an existing item, we're updating the quantity that exists first
+            //this duplicate entry will replace the original map item and in effect update it
+            //if it's a new item, we're simply adding it to our map
+            list.put(item.getName(), item);
+
+            //then we're returning the updated quantity of the item
+            return item.getQuantity();
+        }
+
+        return 0;
+    }
 }
