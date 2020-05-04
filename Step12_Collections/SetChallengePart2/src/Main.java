4cb62d7b7c81a8c15aaf07088e56b61cad5e523d
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-03 21:23:10 -0500

    In Main Class replace all getName methods with getKey method

--- Step12_Collections/SetChallengePart2/src/Main.java
+++ Step12_Collections/SetChallengePart2/src/Main.java
@@ -4,98 +4,98 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Map<HeavenlyBody.Key, HeavenlyBody> solarSystem = new HashMap<>();
     private static Set<HeavenlyBody> planets = new HashSet<>();
 
     public static void main(String[] args) {
         HeavenlyBody temp = new Planet("Mercury", 88);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Venus", 225);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Earth", 365);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         //we're adding the Moon to Earth's satellites HashSet
         HeavenlyBody tempMoon = new Moon("Moon", 27);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon);
 
         temp = new Planet("Mars", 687);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         //we're adding the Moon to Mar's satellites HashSet
         tempMoon = new Moon("Desimos", 1.3);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon);
 
         //we're adding the Moon to Mar's satellites HashSet
         tempMoon = new Moon("Phobos", 0.3);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon);
 
         temp = new Planet("Jupiter", 4332);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         //we're adding the Moon to Jupiter's satellites HashSet
         tempMoon = new Moon("Io", 1.8);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon);
 
         //we're adding the Moon to Jupiter's satellites HashSet
         tempMoon = new Moon("Europa", 3.5);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon);
 
         //we're adding the Moon to Jupiter's satellites HashSet
         tempMoon = new Moon("Ganymede", 7.1);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon);
 
         //we're adding the Moon to Jupiter's satellites HashSet
         tempMoon = new Moon("Callisto", 16.7);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon);
 
         temp = new Planet("Saturn", 10759);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Uranus", 30660);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Neptune", 165);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Pluto", 248);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         System.out.println("Planets");
         for (HeavenlyBody planet : planets) {
-            System.out.println("\t" + planet.getName());
+            System.out.println("\t" + planet.getKey());
         }
         System.out.println();
 
         HeavenlyBody body = solarSystem.get("Jupiter");
-        System.out.println("Moons of " + body.getName());
+        System.out.println("Moons of " + body.getKey());
         for (HeavenlyBody moon : body.getSatellites()) {
-            System.out.println("\t" + moon.getName());
+            System.out.println("\t" + moon.getKey());
         }
         System.out.println();
 
         HeavenlyBody body2 = solarSystem.get("Mars");
-        System.out.println("Moons of " + body2.getName());
+        System.out.println("Moons of " + body2.getKey());
         for (HeavenlyBody moon : body2.getSatellites()) {
-            System.out.println("\t" + moon.getName());
+            System.out.println("\t" + moon.getKey());
         }
 
         //we're adding all of the satellites from each planet to our new moons hashSet
@@ -107,7 +107,7 @@ public class Main {
 
         System.out.println("All Moons");
         for (HeavenlyBody moon : moons) {
-            System.out.println("\t" + moon.getName());
+            System.out.println("\t" + moon.getKey());
         }
 
         //we confirmed that our second pluto planet hasn't overwritten the first pluto planet
