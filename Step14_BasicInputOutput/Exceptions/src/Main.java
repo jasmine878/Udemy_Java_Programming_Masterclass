c6bfaa45ca1900efc74103d31cf7979df39a3d11
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-06 09:23:34 -0500

    Use a try catch block to catch errors

--- Step14_BasicInputOutput/Exceptions/src/Main.java
+++ Step14_BasicInputOutput/Exceptions/src/Main.java
@@ -5,10 +5,20 @@ public class Main {
 
     //lbyl - Look before you leap in coding
     //make sure we're not dividing a number by zero as this will cause an error
-    private static int divide(int x, int y) {
+    private static int divideAttempt1(int x, int y) {
         if (y != 0) {
             return x / y;
         } else {
+            return 0;
+        }
+    }
+
+    //eafp - ask for forgiveness than permission
+    //use a try block to catch any exceptions that would cause an error
+    private static int divideAttempt2(int x, int y) {
+        try {
+            return x / y;
+        } catch(ArithmeticException error) {
             return 0;
         }
     }
