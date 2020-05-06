a643425637a4e56c1c3e1685c495370b7736274f
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-06 18:35:41 -0500

    Place a try catch in our getInt method

--- Step14_BasicInputOutput/Exceptions/src/Example.java
+++ Step14_BasicInputOutput/Exceptions/src/Example.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Example {
@@ -18,6 +19,18 @@ public class Example {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter an integer");
-        return scanner.nextInt();
+
+        //keep asking for input if the input type is invalid
+        while(true) {
+            try {
+                return scanner.nextInt();
+            } catch(InputMismatchException error) {
+                //if error, try again.  but read past the end of line in the input first
+                scanner.nextLine();
+                System.out.println("Please enter a number using only the digits 0 to 9");
+            }
+        }
+
+
     }
 }
