8de60ea2f9b0fd15a50cc9a233d615a046f1ceb2
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-08 16:45:28 -0500

    Create a static initialization block to prevent users from creating more than one instance of the Locations Class

--- Step14_BasicInputOutput/InputOutput_w_Scanner/src/Locations.java
+++ Step14_BasicInputOutput/InputOutput_w_Scanner/src/Locations.java
@@ -10,7 +10,34 @@ import java.util.Set;
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new HashMap<>();
 
+    //this is a static initialization block
+    //static initialization blocks are only executed once when the Locations Class is loaded
+    //this prevents users from creating another instance of the Locations Class
+    static {
+        //the primitive int type gets converted to Integers through auto-boxing!
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
+        locations.put(1, new Location(1,"You are standing at the end of a road before a small brick building"));
+        locations.put(2, new Location(2, "You are at the top of a hill"));
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location(4, "You are in a valley beside a stream"));
+        locations.put(5, new Location(5, "You are in the forest"));
 
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("N", 5);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+
+        locations.get(2).addExit("N", 5);
+
+        locations.get(3).addExit("W", 1);
+
+        locations.get(4).addExit("W", 2);
+        locations.get(4).addExit("N", 1);
+
+        locations.get(5).addExit("W", 2);
+        locations.get(5).addExit("S", 1);
+
+    }
     @Override
     public int size() {
         return locations.size();
